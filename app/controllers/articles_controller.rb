58a77a7545fb6640c7c86b5399b67e4676ffc799
theToxicKnight <michaelomordia.mo@gmail.com>
2021-03-10 15:06:36 -0600

    using flash and adding buttons to simplify webpages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -35,4 +35,14 @@ class ArticlesController < ApplicationController
             render 'edit'
         end
     end
+
+    def destroy
+        @article = Article.find(params[:id])
+        if @article.destroy
+            flash[:notice] = "Article was deleted succesfully"
+            redirect_to @article
+        end
+        
+        flash[:notice]
+    end
 end
