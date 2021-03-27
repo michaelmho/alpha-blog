453c267650fba01d575baa8f9c70c016efdbb0a4
theToxicKnight <michaelomordia.mo@gmail.com>
2021-03-27 16:41:00 -0500

    paginate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,15 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
 
+    def index
+        @users = User.paginate(page: params[:page], per_page: 5)
+    end
+
     def update
         @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Account information was successfully updated"
-            redirect_to articles_path
+            redirect_to @user
 
         else
             render 'edit'
@@ -33,7 +37,7 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
-        @articles = @user.articles
+        @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
 
