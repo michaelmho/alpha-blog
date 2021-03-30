e1bad9e03e3f4388b916bafcf09c45cfd89627d4
theToxicKnight <michaelomordia.mo@gmail.com>
2021-03-30 14:20:06 -0500

    User functionality and admin functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, except: [:show, :index]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def index
         @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -10,7 +12,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
-        @article.user = User.first
+        @article.user = current_user
         if @article.save
             flash[:notice] = "Article was created succesfully"
             redirect_to @article
@@ -36,12 +38,9 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        if @article.destroy
-            flash[:notice] = "Article was deleted succesfully"
-            redirect_to @article
-        end
-        
-        flash[:notice]
+        @article.destroy
+        flash[:notice] = "Article was deleted succesfully"
+        redirect_to @article
     end
 
     private
@@ -55,4 +54,10 @@ class ArticlesController < ApplicationController
         params.require(:article).permit(:title, :description)
     end
 
+    def require_same_user
+        if current_user != @article.user && !current_user.admin?
+            flash[:alert] = "You can only edit or delete your own article"
+            redirect_to @article
+        end
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,27 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, except: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+    
     def new
         @user = User.new
     end
 
     def edit
-        @user = User.find(params[:id])
+        
     end
 
     def index
         @users = User.paginate(page: params[:page], per_page: 5)
     end
 
+    def show
+        
+        @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+    end
+
     def update
-        @user = User.find(params[:id])
+        
         if @user.update(user_params)
             flash[:notice] = "Account information was successfully updated"
             redirect_to @user
@@ -27,6 +36,7 @@ class UsersController < ApplicationController
 
         if @user.save
             name = @user.username
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome, #{name}, to your Alpha Blog account"
             redirect_to articles_path
 
@@ -35,14 +45,27 @@ class UsersController < ApplicationController
         end
     end
 
-    def show
-        @user = User.find(params[:id])
-        @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+    def destroy
+        @user.destroy
+        session[:user_id] = nil if @user == current_user
+        flash[:notice] = "You have succesfully deleted your account"
+        redirect_to user_path
     end
 
 
     private 
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+    
+    def require_same_user
+        if current_user != @user && !current_user.admin?
+            flash[:alert] = "You can only edit or delete your own account"
+            redirect_to @user
+        end
     end
 end
